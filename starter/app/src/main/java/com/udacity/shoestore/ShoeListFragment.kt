6b9f0f4a9a585664867521adfc9acaf19f2444b1
Kristin Risinger <krisinger1@gmail.com>
2020-12-02 00:17:54 -0600

    created shoe list layout with dummy data, added floating action button, added blank detail screen

--- starter/app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
@@ -5,53 +5,61 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.databinding.ShoeBinding
+import com.udacity.shoestore.databinding.ShoeListFragmentBinding
+import com.udacity.shoestore.models.Shoe
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ShoeListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ShoeListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding : ShoeListFragmentBinding
+    private lateinit var itemBinding : ShoeBinding
+
+    private val shoeList : MutableList<Shoe> = mutableListOf(
+        Shoe("name1",5.0,"Nike", "describe shoe 1"),
+        Shoe("name2",6.0,"Addidas", "describe shoe 2"),
+        Shoe("name2",7.0,"Champion", "describe shoe 3"),
+        Shoe("name2",8.0,"Avia", "describe shoe 4")
+    )
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shoe_list, container, false)
+
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.shoe_list_fragment,
+            container,
+            false
+        )
+
+//        var singleShoe = Shoe("cross trainer", 7.0, "Nike", "white athletic shoe" )
+//
+//        binding.shoeListLinearLayout.addView(createShoeItem(inflater, singleShoe))
+
+        for (thisShoe in shoeList){
+            binding.shoeListLinearLayout.addView(createShoeItem(inflater, thisShoe))
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ShoeListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                ShoeListFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+    private fun createShoeItem(inflater: LayoutInflater, shoe : Shoe): View{
+        itemBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.shoe,
+            binding.shoeListLinearLayout,
+            false
+        )
+        itemBinding.shoeBrandText.text = shoe.company
+        itemBinding.shoeDescriptionText.text = shoe.description
+        itemBinding.shoeNameText.text = shoe.name
+        itemBinding.shoeSizeText.text = shoe.size.toString()
+
+
+
+        return itemBinding.root
     }
+
+
 }
